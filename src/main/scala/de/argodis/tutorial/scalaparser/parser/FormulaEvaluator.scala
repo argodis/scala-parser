3ea6cc771f4ccf582702f3f3d8af9dc0424c6e66
venelin <petkov.venelin@gmail.com>
2019-05-14 15:52:21 +0200

    completed evaluator

--- src/main/scala/de/argodis/tutorial/scalaparser/parser/FormulaEvaluator.scala
+++ src/main/scala/de/argodis/tutorial/scalaparser/parser/FormulaEvaluator.scala
@@ -1,12 +1,37 @@
 package de.argodis.tutorial.scalaparser.parser
 
-import de.argodis.tutorial.scalaparser.parser.nodes.FormulaAST
+import de.argodis.tutorial.scalaparser.parser.nodes.{Constant, FormulaAST, OperatorAdd, OperatorDivide, OperatorMultiply, OperatorSubtract, Variable}
 
 object FormulaEvaluator {
-  def evaluate(formulaTree: FormulaAST, input: Map[Int, Option[Double]]): Option[Double] = {
+
+  // Handles problematic floating point values
+  def sanitize(x: Double): Option[Double] = x match {
+    case value if java.lang.Double.isNaN(value) => None
+    case Double.PositiveInfinity => None
+    case Double.NegativeInfinity => None
+    case value => Some(value)
+  }
+
+  // Take options as input, return an option as output
+  def safeOp(left: Option[Double], right: Option[Double], op: (Double, Double) => Double): Option[Double] = {
+    val result = for {
+      x <- left
+      y <- right
+    } yield op(x, y)
+    // We don't want to deal with NaN, Infinity, etc
+    result.flatMap(sanitize)
+  }
+
+  def evaluate(formulaTree: FormulaAST, input: Map[Int, Double]): Option[Double] = {
 
     // Calculate numerical expressions
     def calculate(node: FormulaAST): Option[Double] = node match {
+      case Constant(value) => Some(value)
+      case Variable(id) => input.get(id)
+      case OperatorAdd(left, right) => safeOp(calculate(left), calculate(right), _ + _)
+      case OperatorSubtract(left, right) => safeOp(calculate(left), calculate(right), _ - _)
+      case OperatorMultiply(left, right) => safeOp(calculate(left), calculate(right), _ * _)
+      case OperatorDivide(left, right) => safeOp(calculate(left), calculate(right), _ / _)
       case _ => None
     }
 
--- src/test/scala/TestFormulaEvaluator.scala
+++ src/test/scala/TestFormulaEvaluator.scala
@@ -1,25 +1,92 @@
 import de.argodis.tutorial.scalaparser.parser.{FormulaEvaluator, FormulaParser}
-//import de.argodis.tutorial.scalaparser.parser.nodes._
 import org.scalatest.{FunSuite, Matchers}
 
 class TestFormulaEvaluator extends FunSuite with Matchers {
-//  val SIMPLE_FORMULA = "($1 + $2) / ($3 - $4) + 100 * $5"
-
-//  test("parser should produce the expression '(($1 + 2) * ($3 + 4)) / ($5 + 6)' ") {
-//    val factor1 = OperatorAdd(Variable(1), Constant(2))
-//    val factor2 = OperatorAdd(Variable(3), Constant(4))
-//    val numerator = OperatorMultiply(factor1, factor2)
-//    val denominator = OperatorAdd(Variable(5), Constant(6))
-//    val tree = OperatorDivide(numerator, denominator)
-//    FormulaParser.parse("(($1 + 2) * ($3 + 4)) / ($5 + 6)") shouldBe Right(tree)
-//  }
-  test("evaluation expression '1'") {
+  test("evaluate expression '1'") {
     FormulaParser
       .parse("1")
       .map(ast => FormulaEvaluator.evaluate(ast, Map()))
-      .foreach(result => {
-        println(s"--- RESULT ---")
-        println(s"result: $result")
-      })
+      .map(result => result shouldBe Some(1.0))
   }
+
+  test("evaluate expression '$1' where '$1 = -4.25' ") {
+    FormulaParser
+      .parse("$1")
+      .map(ast => FormulaEvaluator.evaluate(ast, Map(1 -> -4.25)))
+      .map(result => result shouldBe Some(-4.25))
+  }
+
+  test("convert a proper double to Some(value)") {
+    FormulaEvaluator.sanitize(0.25)  shouldBe Some(0.25)
+  }
+
+  test("convert a scala.Double.NaN to None") {
+    FormulaEvaluator.sanitize(scala.Double.NaN)  shouldBe None
+  }
+
+  test("convert a positive infinity to None") {
+    val result = Some(1.25).map(value => value / 0.0)
+    FormulaEvaluator.sanitize(result.get) shouldBe None
+  }
+
+  test("convert a negative infinity to None") {
+    val result = Some(1.25).map(value => - value / 0.0)
+    FormulaEvaluator.sanitize(result.get) shouldBe None
+  }
+
+  test("safeOp returns a result when both arguments are defined") {
+    FormulaEvaluator.safeOp(Some(1), Some(1), _ + _) shouldBe Some(2)
+  }
+
+  test("safeOp returns None when the left argument is None") {
+    FormulaEvaluator.safeOp(None, Some(1), _ + _) shouldBe None
+  }
+
+  test("safeOp returns None when the right argument is None") {
+    FormulaEvaluator.safeOp(Some(1), None, _ + _) shouldBe None
+  }
+
+  test("safeOp returns None when both arguments are None") {
+    FormulaEvaluator.safeOp(None, None, _ + _) shouldBe None
+  }
+
+  test("safeOp returns None when a division by zero occurs") {
+    FormulaEvaluator.safeOp(Some(0.25), Some(0.0), _ / _) shouldBe None
+  }
+
+  test("evaluate expression '$1 - 1'") {
+    FormulaParser
+      .parse("$1 - 1")
+      .map(ast => FormulaEvaluator.evaluate(ast, Map(1 -> 1)))
+      .map(result => result shouldBe Some(0.0))
+  }
+
+  test("evaluate expression '$1 - $1'") {
+    FormulaParser
+      .parse("$1 - $1")
+      .map(ast => FormulaEvaluator.evaluate(ast, Map(1 -> 1)))
+      .map(result => result shouldBe Some(0.0))
+  }
+
+  test("evaluate expression '$1 - $2'") {
+    FormulaParser
+      .parse("$1 - $2")
+      .map(ast => FormulaEvaluator.evaluate(ast, Map(1 -> 1, 2 -> 1)))
+      .map(result => result shouldBe Some(0.0))
+  }
+
+  test("evaluate expression with a missing second argument '$1 - $2'") {
+    FormulaParser
+      .parse("$1 - $2")
+      .map(ast => FormulaEvaluator.evaluate(ast, Map(1 -> 1)))
+      .map(result => result shouldBe None)
+  }
+
+  test("evaluate expression '($1 + $2) * ($3 + $4)'") {
+    FormulaParser
+      .parse("($1 + $2) * ($3 + $4)")
+      .map(ast => FormulaEvaluator.evaluate(ast, Map(1 -> 1, 2 -> 1, 3 -> 3, 4 -> 2)))
+      .map(result => result shouldBe Some(10.0))
+  }
+
 }
