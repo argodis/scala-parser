cc118715eecbe4c20d30e5913f8f7077fe6c64d7
venelin <petkov.venelin@gmail.com>
2019-05-15 17:01:08 +0200

    Added a stub for the parse function

--- src/main/scala/de/argodis/tutorial/scalaparser/FormulaParserApp.scala
+++ src/main/scala/de/argodis/tutorial/scalaparser/FormulaParserApp.scala
@@ -1,5 +1,6 @@
 package de.argodis.tutorial.scalaparser
 import com.beust.jcommander.JCommander
+import de.argodis.tutorial.scalaparser.parser.nodes.FormulaAST
 import de.argodis.tutorial.scalaparser.schema.{FormulaRow, InputDataRow}
 
 object FormulaParserApp {
@@ -15,14 +16,29 @@ object FormulaParserApp {
     arguments
   }
 
-  def process(input: List[InputDataRow], trees: List[FormulaRow]): Either[String, List[String]] = Right(List())
+  def parse(formulas: List[FormulaRow]): Either[String, List[(Long, FormulaAST)]] = Right(List())
+//    // Attempt to parse all formulas
+//    //    val trees = formulas.map(formulaRow => FormulaParser.parse2(formulaRow.formula))
+//    // We need to preserve the formula ids, so that we can map them
+//    val trees = formulas.map(formulaRow => FormulaParser.parse2(formulaRow.formula).map(ast => (formulaRow.id, ast)))
+//
+//    // Look for errors and return the first error found. Otherwise, return the list of formula ASTs, sorted by id:
+//    trees collectFirst {
+//      case Left(error) => error
+//    } toLeft {
+//      trees collect { case Right(r) => r }
+//    }.sortBy(_._1)
+//  }
+
+  def process(input: List[InputDataRow], trees: List[(Long, FormulaAST)]): Either[String, List[String]] = Right(List())
 
   def main(args: Array[String]): Unit = {
     val arguments = parseCliArguments(args)
     val result = for {
       input <- Data.loadInput(arguments.input)
       formulas <- Data.loadFormula(arguments.formula)
-      output <- process(input, formulas)
+      trees <- parse(formulas)
+      output <- process(input, trees)
       msg <- Data.writeOutput(arguments.output, output)
     } yield msg
 
